ab3a6b818019cd647beafb509fef089fe52c3589
Kelvin Ben <kelvinbandeira2003@gmail.com>
2026-02-24 23:01:55 -0300

    organiza código em funções e aplica boas práticas

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-print('----------------------------- Calculadora de Gorjeta -----------------------------')
+# Função que calcula a gorjeta:
+def calcula_gorjeta(valor_conta, porcentagem_gorjeta):
+    valor_gorjeta = valor_conta * porcentagem_gorjeta / 100
+    total_final = valor_conta + valor_gorjeta
+    return valor_gorjeta, total_final
 
-# Entradas do usuário:
-valor_conta = float(input("Digite o valor da conta: R$"))
-porcentagem_gorjeta = float(input("Digite a porcentagem da gorjeta: "))
 
-# Cálculo do valor da gorjeta:
-valor_gorjeta = valor_conta * porcentagem_gorjeta / 100
-total_final = valor_conta + valor_gorjeta
+# Função principal:
+def main():
+    print("===================== Calculadora de Gorjeta ====================")
+    valor_conta = float(input('Digite o valor da conta: R$'))
+    porcentagem_gorjeta = float(input('Digite a porcentagem da gorjeta: '))
+    print("=" * 65)
+    valor_gorjeta, total_final = calcula_gorjeta(valor_conta, porcentagem_gorjeta)
+    print(f"O valor da gorjeta é de: R${valor_gorjeta:.2f}")
+    print(f"O total final da conta do cliente é de: R${total_final:.2f}")
+    print("=" * 65)
 
-print('----------------------------------------------------------------------------------')
 
-# Impressão do resultado:
-print(f"O valor da gorjeta é de: R${valor_gorjeta:.2f}")
-print(f"O total final da conta do cliente é de: R${total_final:.2f}")
-
-print('----------------------------------------------------------------------------------')
+# Proteção padrão
+if __name__ == "__main__":
+    main()
