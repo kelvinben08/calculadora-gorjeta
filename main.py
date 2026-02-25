e221b0902dd44697e644c412446eb728f373ee62
Kelvin Ben <kelvinbandeira2003@gmail.com>
2026-02-24 23:48:42 -0300

    adiciona validação de entrada e tratamento de erros

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 # Função que calcula a gorjeta:
-def calcula_gorjeta(valor_conta, porcentagem_gorjeta):
+def calcular_gorjeta(valor_conta, porcentagem_gorjeta):
     valor_gorjeta = valor_conta * porcentagem_gorjeta / 100
     total_final = valor_conta + valor_gorjeta
     return valor_gorjeta, total_final
 
 
+# Função que solicita o valor:
+def solicitar_valor(mensagem):
+    while True:
+        try:
+            valor = float(input(mensagem))
+            if valor >= 0:
+                return valor
+            else:
+                print("O valor digitado deve ser maior que 0. Tente novamente.")
+        except ValueError:
+            print("Valor digitado inválido. Tente novamente.")
+
+
 # Função principal:
 def main():
-    print("===================== Calculadora de Gorjeta ====================")
-    valor_conta = float(input('Digite o valor da conta: R$'))
-    porcentagem_gorjeta = float(input('Digite a porcentagem da gorjeta: '))
     print("=" * 65)
-    valor_gorjeta, total_final = calcula_gorjeta(valor_conta, porcentagem_gorjeta)
+    print("Calculadora de Gorjeta")
+    print("=" * 65)
+    valor_conta = solicitar_valor('Digite o valor da conta: R$')
+    porcentagem_gorjeta = solicitar_valor('Digite a porcentagem da gorjeta: ')
+    print("=" * 65)
+    valor_gorjeta, total_final = calcular_gorjeta(valor_conta, porcentagem_gorjeta)
     print(f"O valor da gorjeta é de: R${valor_gorjeta:.2f}")
     print(f"O total final da conta do cliente é de: R${total_final:.2f}")
     print("=" * 65)
